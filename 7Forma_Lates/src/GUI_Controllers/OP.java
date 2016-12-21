46b6241d2b7fb87b5f7b50bf89379680aead5d2d
802499 <802499@Samsung>
2016-12-21 21:02:16 +0300

    Change-Id: I9774596b351691a3d36027fe4862aa6f7d35f2b9 Signed-off-by: 802499 <802499@Samsung>

--- 7Forma_Lates/src/GUI_Controllers/OP.java
+++ 7Forma_Lates/src/GUI_Controllers/OP.java
@@ -23,6 +23,12 @@ import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.view.JasperViewer;
 
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+
 public class OP implements Initializable{
 	private Stage thisdialogstage;
 	
@@ -47,20 +53,27 @@ public class OP implements Initializable{
 			getdialogstage().close();			
 		}
 	}
-	@FXML public void on_OK(ActionEvent event) throws IOException, JRException{
+	@FXML public void on_OK(ActionEvent event) throws IOException{
 		
 		//String sourceFileName="/home/demonaz/workspace/7Forma_Lates/src/Otchet/Invoice.jrxml";
 		String sourceFileName="C:\\Users\\802499\\git\\7FORMA\\7Forma_Lates\\src\\Otchet\\Invoice.jrxml";
 		//String sourceFileName="C:/Users/802499/git/7FORMA/7Forma_Lates/src/Otchet/Invoice.jrxml";
 		JOptionPane.showMessageDialog(null, sourceFileName);
-		JasperReport jasperReport = JasperCompileManager.compileReport(sourceFileName);		
-		Map<String, Object> parameters = new HashMap<String, Object>();
-		parameters.put("Data_IN", Data_IN.getValue().toString());
-		parameters.put("Data_OUT", Data_OUT.getValue().toString());		
-		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport,parameters,SQLiteJDBC.Get_Conn());
-		JasperViewer visor = new JasperViewer(jasperPrint, false);
+		JasperReport jasperReport;
+		try {
+			jasperReport = JasperCompileManager.compileReport(sourceFileName);
+			
+			Map<String, Object> parameters = new HashMap<String, Object>();
+			parameters.put("Data_IN", Data_IN.getValue().toString());
+			parameters.put("Data_OUT", Data_OUT.getValue().toString());		
+			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport,parameters,SQLiteJDBC.Get_Conn());
+			JasperViewer visor = new JasperViewer(jasperPrint, false);
 		
-		visor.setVisible(true);
+			visor.setVisible(true);
+		} catch (JRException e) {
+			System.out.println(e.getMessage());
+		}
+		
 	}
 
 }
